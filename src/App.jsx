8147c0e5bdde660f09f8bb0a4846291336cee55a
gusvianadev <gusvianadev@gmail.com>
2021-08-06 12:06:15 -0300

    feature: the entire calculator
    now the only thing left is making the code cleaner if able

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,29 @@
-const App = () => <div />;
+import { ThemeProvider } from 'styled-components';
+import AppFunctions from './App.functions';
+import AppSty from './styles/App.styles';
+import CalcSectionBackgroundSty from './styles/CalcSectionBackground.styles';
+import GlobalStyles from './styles/GlobalStyles.styles';
+
+const App = () => {
+	const { calcItems, theme } = AppFunctions();
+
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<AppSty>
+				<div className="calculator">
+					{calcItems.map(({ name, background, content }) => (
+						<CalcSectionBackgroundSty
+							key={name}
+							bgColor={background}
+						>
+							{content}
+						</CalcSectionBackgroundSty>
+					))}
+				</div>
+			</AppSty>
+		</ThemeProvider>
+	);
+};
+
 export default App;
